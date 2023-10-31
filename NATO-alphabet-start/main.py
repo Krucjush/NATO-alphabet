8df500f8911edc2ce893f0ecf965370a3424bc76
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-10-31 21:31:38 +0100

    Handle situation when user inputs character that is not a letter

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -3,5 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic_alphabet_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-print([nato_phonetic_alphabet_dictionary[letter] for letter in word])
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        print([nato_phonetic_alphabet_dictionary[letter] for letter in word])
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+
+
+generate_phonetic()
